0f08bd70ac5fdf5f733c7de129e4f8f97c72371a
poosarla-sri-vijay-kalki <srivijaykalki@gmail.com>
2024-03-11 16:27:13 -0400

    UPDATE google Analytics code

--- frontend_react/src/index.js
+++ frontend_react/src/index.js
@@ -1,7 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 
 import App from './App';
 import './index.css';
+import ReactGA from "react-ga4";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactGA.initialize(process.env.REACT_APP_GOOGLE_ANALYTICS_MESUREMENT_ID);
+ReactGA.send({ hitType: "pageview", page: window.location.pathname});
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+);
